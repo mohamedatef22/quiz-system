d118aed98580708a550935d163463dad64f534af
Mohamed Atef <mohamedatef2002m@yahoo.com>
2021-07-03 13:17:17 +0200

    edit seed file:

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // drop all records
         User::truncate();
         Question::truncate();
         Answer::truncate();
@@ -28,44 +29,58 @@ class DatabaseSeeder extends Seeder
         room_user::truncate();
         user_quiz::truncate();
 
+        // Create users  for database. user1 is the instructor to create rooms
+        // users is the students
         $user1 = User::factory()->create();
         $users = User::factory(5)->create();
         // $user3 = User::factory()->create();
         // $user4 = User::factory()->create();
 
+        //  create 5 rooms
         $rooms = Room::factory()->count(5)->create([
             'user_id' => $user1,
         ]);
 
+        // loop on rooms
         foreach ($rooms as $room) {
+
+            // create two quizzes for the room
             $quizzes = Quiz::factory(2)->create([
                 'room_id' => $room,
             ]);
 
+            // enroll all users in the room
             for ($i = 0; $i < 5; $i++) {
                 room_user::factory()->create([
                     'user_id' => $users[$i],
                     'room_id' => $room,
                 ]);
             }
+
+            // loop on quizzes
             foreach ($quizzes as $quiz) {
 
+                // assign user to quiz that he solve
                 for ($i = 0; $i < 5; $i++) {
                     user_quiz::factory()->create([
                         'user_id' => $users[$i],
                         'quiz_id' => $quiz,
                     ]);
                 }
+
+                // add questions to quiz
                 $questions = Question::factory(10)->create([
                     'quiz_id' => $quiz,
                 ]);
+
+                // loop on question to add answers
+                foreach ($questions as $question) {
+                    Answer::factory(5)->create([
+                        'question_id' => $question,
+                    ]);
+                }
             }
 
-            foreach ($questions as $question) {
-                Answer::factory(5)->create([
-                    'question_id' => $question,
-                ]);
-            }
         }
 
     }
