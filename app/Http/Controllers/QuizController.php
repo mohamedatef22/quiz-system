c7741441fa4c0755fc3d32cbfb077a064f041d2c
Mohamed Atef <mohamedatef2002m@yahoo.com>
2021-07-07 21:02:09 +0200

    apply middleware

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -13,21 +13,23 @@ class QuizController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        // $this->middleware('room.enrolled');
+        $this->middleware('quiz.nottaken');
     }
 
     public function show(Quiz $quiz)
     {
-        $room_id = $quiz->room_id;
-        $quiz_id = $quiz->id;
-        $user_id = Auth::user()->id;
-        $is_enrolled_in_room = room_user::where('user_id', $user_id)->where('room_id', $room_id)->get();
-        if (!$is_enrolled_in_room->contains('user_id', $user_id)) {
-            return redirect()->route('home')->with('msg', 'not authrized to get show quiz');
-        }
-        $is_quiz_taken = user_quiz::where('user_id', $user_id)->where('quiz_id', $quiz_id)->get();
-        if ($is_quiz_taken->contains('user_id', $user_id)) {
-            return redirect()->back()->with('msg', 'can\'t take quiz again');
-        }
+        // $room_id = $quiz->room_id;
+        // $quiz_id = $quiz->id;
+        // $user_id = Auth::user()->id;
+        // $is_enrolled_in_room = room_user::where('user_id', $user_id)->where('room_id', $room_id)->get();
+        // if (!$is_enrolled_in_room->contains('user_id', $user_id)) {
+        //     return redirect()->route('home')->with('msg', 'not authrized to get show quiz');
+        // }
+        // $is_quiz_taken = user_quiz::where('user_id', $user_id)->where('quiz_id', $quiz_id)->get();
+        // if ($is_quiz_taken->contains('user_id', $user_id)) {
+        //     return redirect()->back()->with('msg', 'can\'t take quiz again');
+        // }
         return view('quiz.show', compact('quiz'));
     }
 
