3f5934fc110145b5eb4064bbffc9cf212958e475
Mohamed Atef <mohamedatef2002m@yahoo.com>
2021-07-10 21:37:33 +0200

    fix ended_at current time stamp

--- database/migrations/2021_07_02_132330_create_user_quiz_table.php
+++ database/migrations/2021_07_02_132330_create_user_quiz_table.php
@@ -18,7 +18,7 @@ class CreateUserQuizTable extends Migration
             $table->foreignId('quiz_id');
             $table->unsignedInteger('grade')->default(0);
             $table->timestamp('started_at')->useCurrent();
-            $table->timestamp('ended_at')->default(now());
+            $table->timestamp('ended_at')->nullable();
 
             $table->unique(['user_id', 'quiz_id']);
         });
